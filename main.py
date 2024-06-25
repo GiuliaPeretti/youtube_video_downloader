205a58fd9211d1a97b0845e7ac6c0e958ed85e1c
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-25 20:23:54 +0200

    finito

--- main.py
+++ main.py
@@ -3,39 +3,57 @@ from pytube import YouTube
 from pathlib import Path
 import os
 
-def download(url, type, dir_path):
-    print(type)
-    if(dir_path==""):
+def download_links(urls, dir_path, type):
+    urls=urls.split("\n")
+    for i in range (len(urls)):
+        if(type=='video'):
+            try:
+                yt =YouTube(urls[i])
+                video = yt.streams.get_highest_resolution()
+                video.download(filename=f"{video.title}.mp4", output_path=dir_path)
+                # return("The video "+str(i)+" is downloaded in MP4")
+            except KeyError:
+                return("Unable to fetch video information. Please check the video URL or your network connection.")
+        else:
+            try:
+                video = YouTube(urls[i])
+                stream = video.streams.filter(only_audio=True).first()
+                stream.download(filename=f"{video.title}.mp3", output_path=dir_path)
+                # return("The video "+str(i)+" is downloaded in MP3")
+            except KeyError:
+                return("Unable to fetch video information. Please check the video URL or your network connection.")
+
+def download(link_audio, link_video, dir_video, dir_audio):
+    if(dir_video==""):
         return("Please insert the directory path")
-    if (len(type)!=1):
-        return("Please choose one type")
-    elif(type[0]=='video'):
-        yt =YouTube(url)
-        # video = yt.streams.filter(only_audio=True).first() 
-        video = yt.streams.get_highest_resolution()
-        video.download(filename=f"{video.title}.mp4", output_path=dir_path)
-        return("video")
+
     else:
-        try:
-            video = YouTube(url)
-            stream = video.streams.filter(only_audio=True).first()
-            stream.download(filename=f"{video.title}.mp3", output_path=dir_path)
-            return("The video is downloaded in MP3")
-        except KeyError:
-            return("Unable to fetch video information. Please check the video URL or your network connection.")
+        if(link_audio=="" and link_video==""):
+            return("Please insert at least one link", )
+        if(link_audio!=""):
+            download_links(link_audio, dir_audio, 'audio')
+        if(link_video!=""):
+            download_links(link_video, dir_video, 'video')
+        return("All done")
+    
 
 if __name__=='__main__':
 
     with gr.Blocks() as demo:
 
         gr.Markdown("# Youtube video downloader")
-        type=gr.CheckboxGroup(label="Audio:", choices=['audio', 'video'])
-        link=gr.Textbox(label="Link:")
-        dir_path=gr.Textbox(label="Directory path:")
+        with gr.Row():
+            link_audio=gr.TextArea(label="Download mp3")
+            link_video=gr.TextArea(label="Download mp4")
+        with gr.Row():
+            dir_audio=gr.Textbox(label="Directory path:")
+            dir_video=gr.Textbox(label="Directory path:")
         b_download=gr.Button("Download")
-        result=gr.Textbox("")
+        with gr.Row():
+            result=gr.Textbox("", label="Result: ")
+            # result_video=gr.Textbox("", label="Result video: ")
 
-        b_download.click(fn=download, inputs=[link, type, dir_path], outputs=[result])
+        b_download.click(fn=download, inputs=[link_audio, link_video, dir_audio, dir_video], outputs=[result])
 
 
     demo.launch(share=False)
