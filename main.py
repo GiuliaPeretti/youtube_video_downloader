ebfeadccfab20d883bc600b899397077a745b208
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-25 17:29:28 +0200

    video and audio works

--- main.py
+++ main.py
@@ -2,35 +2,36 @@ import gradio as gr
 from pytube import YouTube
 import os
 
-def download(link):
-    print(link)
-    link=str(link)
-    yt =YouTube(str(link))
-    video = yt.streams.filter(only_audio=True).first() 
-    video.download()
-    out_file = video.download(output_path="C:\\Users\\giuli\\Documents\\GitHub\\youtube_video_downloader\\video\\") 
-  
-    # save the file 
-    base, ext = os.path.splitext(out_file) 
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file) 
-    print(out_file)
-    os.remove(out_file)
-
-    return("Fatto")
+def download(url, type):
+    print(type)
+    if (len(type)!=1):
+        return("Please choose one type")
+    elif(type[0]=='video'):
+        yt =YouTube(url)
+        # video = yt.streams.filter(only_audio=True).first() 
+        video = yt.streams.get_highest_resolution()
+        video.download(filename=f"{video.title}.mp3")
+        return("video")
+    else:
+        try:
+            video = YouTube(url)
+            stream = video.streams.filter(only_audio=True).first()
+            stream.download(filename=f"{video.title}.mp3")
+            return("The video is downloaded in MP3")
+        except KeyError:
+            return("Unable to fetch video information. Please check the video URL or your network connection.")
 
 if __name__=='__main__':
 
     with gr.Blocks() as demo:
 
         gr.Markdown("# Youtube video downloader")
-        audio_type=gr.CheckboxGroup(label="Audio:", choices=['mp3'])
-        video_type=gr.CheckboxGroup(label="Video:", choices=['mp4'])
+        type=gr.CheckboxGroup(label="Audio:", choices=['audio', 'video'])
         link=gr.Textbox(label="Link:")
         b_download=gr.Button("Download")
         result=gr.Textbox("")
 
-        b_download.click(fn=download, inputs=[link], outputs=[result])
+        b_download.click(fn=download, inputs=[link, type], outputs=[result])
 
 
     demo.launch(share=False)
