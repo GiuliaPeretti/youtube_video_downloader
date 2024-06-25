95864b4d3f21669d1f99ce831f6ed3125a2eeca2
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-25 16:56:59 +0200

    trying to download

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import gradio as gr
+from pytube import YouTube
+import os
 
-def download(link)
+def download(link):
+    print(link)
+    link=str(link)
+    yt =YouTube(str(link))
+    video = yt.streams.filter(only_audio=True).first() 
+    video.download()
+    out_file = video.download(output_path="C:\\Users\\giuli\\Documents\\GitHub\\youtube_video_downloader\\video\\") 
+  
+    # save the file 
+    base, ext = os.path.splitext(out_file) 
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file) 
+    print(out_file)
+    os.remove(out_file)
+
+    return("Fatto")
 
 if __name__=='__main__':
 
@@ -10,9 +27,10 @@ if __name__=='__main__':
         audio_type=gr.CheckboxGroup(label="Audio:", choices=['mp3'])
         video_type=gr.CheckboxGroup(label="Video:", choices=['mp4'])
         link=gr.Textbox(label="Link:")
-        b_download=gr.Button("Summarizer")
+        b_download=gr.Button("Download")
+        result=gr.Textbox("")
 
-        b_download.click(fn=speech_to_text, inputs=[file_upload, check], outputs=[text_result])
+        b_download.click(fn=download, inputs=[link], outputs=[result])
 
 
     demo.launch(share=False)
